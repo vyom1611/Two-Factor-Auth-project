b19850fd242384de4c486c7dd83f95108c5bfce1
vyom <sharm843@umn.edu>
2022-07-16 00:45:24 +0530

    Secret Token generator added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { config } = require("node-json-db/dist/lib/JsonDBConfig");
 
 App = express();
 
+App.use(express.json)
+
 const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
 App.get("/api", (req, res) => {
@@ -15,7 +17,32 @@ App.get("/api", (req, res) => {
 })
 
 //Register user and create temporary secret
+App.post('/api/register', (req,res) => {
+    const id = uuid.v4()
 
+    try {
+        const path = `/user/${id}`
+
+        db.push(path, { id })
+        const temp_secret = speakeasy.generateSecret()
+        res.json({ id, secret: temp_secret.base32 })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error generating secret"})
+    }
+})
+
+
+//Verify token and make secret permanent
+App.post('/api/verify', (req,res) => {
+    const { token, userID } = req.body
+    try {
+        const path = `/user/${userID}`
+        const user = db.getData(path)
+    } catch (error) {
+        
+    }
+})
 
 const PORT = process.env.PORT || 5000;
 
